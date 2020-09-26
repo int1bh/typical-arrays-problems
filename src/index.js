24785ed7f63a5400720b40aa9ceb1314161cf5d9
ALEXANDR <alexandrdryagin@yandex.ru>
2020-09-26 20:03:02 +0300

    complite

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array) return 0;
+  let minimal = array.sort(function(a, b) { return a - b; });
+  return minimal[0] || 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array) return 0;
+  let maximal = array.sort(function(a, b) { return a - b; });
+  return maximal.reverse()[0] || 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array) return 0;
+  let sum = array.reduce(function(a, b) { return a + b; }, 0);
+  return (sum / array.length) || 0;
 }
